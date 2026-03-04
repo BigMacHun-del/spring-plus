98e3114458be1e6d092744c2e4ba4b3e10ed37da
김대훈 <eogns136@gmail.com>
2026-03-04 09:56:50 +0900

    refactor: Transactional 누락 수정

--- src/main/java/org/example/expert/domain/todo/service/TodoService.java
+++ src/main/java/org/example/expert/domain/todo/service/TodoService.java
@@ -53,6 +53,7 @@ public class TodoService {
         );
     }
 
+    @Transactional(readOnly = true)
     public Page<TodoResponse> getTodos(int page, int size, String weather, String startDate, String endDate) {
         Pageable pageable = PageRequest.of(page - 1, size);
 
@@ -81,6 +82,7 @@ public class TodoService {
         ));
     }
 
+    @Transactional(readOnly = true)
     public TodoResponse getTodo(long todoId) {
         Todo todo = todoRepository.findByIdWithUser(todoId)
                 .orElseThrow(() -> new InvalidRequestException("Todo not found"));
@@ -98,6 +100,7 @@ public class TodoService {
         );
     }
 
+    @Transactional
     public Page<TodoSearchResponse> searchTodos(
             int page, int size,
             String title,
